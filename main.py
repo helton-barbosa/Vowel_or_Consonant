da0b63b0a7b472d1f2f8b472ea48181c422d82a2
Helton Barbosa <heltonbs@gmail.com>
2023-12-30 12:16:39 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+ch = input('Entre com a letra: ').capitalize()
+
+if ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U':
+    print(f'A letra {ch} é uma vogal.')
+else:
+    print(f'A letra {ch} é uma consoante.')
